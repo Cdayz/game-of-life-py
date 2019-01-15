16d2f2018b9d58ce0d5ec6177905825a982670ea
Cdayz <cdayz@yandex.ru>
2019-01-15 15:45:38 +0300

    refactor(image): Fixing linter warnings

--- gol/img_backend/pillow_backend.py
+++ gol/img_backend/pillow_backend.py
@@ -2,7 +2,6 @@
 Module wich represent backend for saving states into images using PIL
 """
 
-import pathlib
 from typing import Tuple, List
 
 from PIL import Image
@@ -17,11 +16,15 @@ class ImgBackend:
     """
     Class wich represents low-level image operations as backend to GoL
     """
-    def __init__(self, img_path: str,
-                 size: StateSize, cell_size: CellSize, line_width: int = 1):
+
+    __slots__ = ('width', 'height', 'img_path', 'cell_w', 'cell_h',
+                 'img_w', 'img_h', 'line_width')
+
+    def __init__(self, size: StateSize,
+                 cell_size: CellSize, line_width: int = 1):
         """
         Initialize class and calculate needed values
-        
+
         :param str img_path: path to image (may be relative)
         :param size: size of state
         :type size: Tuple[int, int]
@@ -30,7 +33,6 @@ class ImgBackend:
         :param int line_width: width of line in pixels
         """
         self.width, self.height = size
-        self.img_path = pathlib.Path(img_path).absolute()
         self.cell_w, self.cell_h = cell_size
         self.img_w = (self.cell_w + line_width) * self.width + line_width
         self.img_h = (self.cell_h + line_width) * self.height + line_width
@@ -49,13 +51,13 @@ class ImgBackend:
         x_step = self.cell_w
         y_step = self.cell_h
 
-        for x in range(0, self.img_w, x_step):
-            for y in range(0, self.img_h, y_step):
-                image.putpixel((x, y), 180)
+        for x_index in range(0, self.img_w, x_step):
+            for y_index in range(0, self.img_h, y_step):
+                image.putpixel((x_index, y_index), 180)
 
-        for x in range(0, self.img_h, x_step):
-            for y in range(0, self.img_w, y_step):
-                image.putpixel((x, y), 180)
+        for x_index in range(0, self.img_h, x_step):
+            for y_index in range(0, self.img_w, y_step):
+                image.putpixel((x_index, y_index), 180)
 
         return image
 
@@ -73,9 +75,9 @@ class ImgBackend:
         if len(state) != self.width or len(state[0]) != self.height:
             raise Exception('State size differ from initial!')
 
-        for x in range(self.width):
-            for y in range(self.height):
-                img = self.put_value(img, (x, y), state)
+        for x_index in range(self.width):
+            for y_index in range(self.height):
+                img = self.put_value(img, (x_index, y_index), state)
 
         return img
 
@@ -93,15 +95,15 @@ class ImgBackend:
         :return: Image with pixels set
         :rtype: `~.Image`
         """
-        xp, yp = point
-        value = 0 if state[xp][yp] else 255
+        x_point, y_point = point
+        value = 0 if state[x_point][y_point] else 255
 
-        x_start = (self.cell_h + self.line_width) * xp + self.line_width
-        y_start = (self.cell_w + self.line_width) * yp + self.line_width
+        x_start = (self.cell_h + self.line_width) * x_point + self.line_width
+        y_start = (self.cell_w + self.line_width) * y_point + self.line_width
 
 
-        for x in range(x_start, x_start + self.cell_h):
-            for y in range(y_start, y_start + self.cell_w):
-                img.putpixel((y,x), value)
+        for x_index in range(x_start, x_start + self.cell_h):
+            for y_index in range(y_start, y_start + self.cell_w):
+                img.putpixel((y_index, x_index), value)
 
         return img
