2b286cd30de2aa097a23a4e5f50aaede2c44e6a1
Cdayz <cdayz@yandex.ru>
2019-01-15 15:45:59 +0300

    refactor(test): Fixing test file after refactoring

--- test.py
+++ test.py
@@ -2,17 +2,17 @@ from gol.logics import next_state
 from gol.img_backend.pillow_backend import ImgBackend
 
 def create_image(state):
-    bc = ImgBackend('test.png', (len(state), len(state[0])), (1, 1),
+    bc = ImgBackend((len(state), len(state[0])), (1, 1),
                     line_width=1)
     img = bc.to_image(state)
 
-    img.save(bc.img_path.as_posix())
+    img.save('test.png')
 
 
 def create_gif(initial_state, path, frames=10, fps=3):
     state_size = (len(initial_state), len(initial_state[0]))
     cell_size = (1, 1)
-    bc = ImgBackend(path, state_size, cell_size, line_width=1)
+    bc = ImgBackend(state_size, cell_size, line_width=1)
     all_states = [initial_state, ]
     state = initial_state
     for _ in range(frames):
@@ -21,7 +21,7 @@ def create_gif(initial_state, path, frames=10, fps=3):
 
     images = [bc.to_image(state) for state in all_states]
 
-    images[0].save(bc.img_path.as_posix(),
+    images[0].save(path,
                    save_all=True,
                    append_images=images[1:],
                    duration=len(images) / fps,
